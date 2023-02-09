715017190b977bec3f6368fa8f81720c2cd7f1cb
elamparethi <elamparethi02a2gmail.com>
2023-02-09 15:37:21 +0530

    Added : StopWatch java program

---
+++ StopWatch.java
@@ -0,0 +1,21 @@
+package com.Logicalprogram;
+
+import java.util.Scanner;
+
+public class StopWatch {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        long start, end;
+        double elapsedtime;
+        System.out.println("Enter any character to start the timer: ");
+        char x = scanner.next().charAt(0);
+        start = System.currentTimeMillis();
+        System.out.println("Enter any character to stop the timer: ");
+        char y = scanner.next().charAt(0);
+        end = System.currentTimeMillis();
+        elapsedtime = (end - start)/1000.0;
+        System.out.println(elapsedtime);
+    }
+
+}
